de1978da2e79789e56f22867ee734daa4232f76c
Serguei <blackraven36@gmail.com>
2017-11-19 17:51:05 -0600

    Added sub modules to setup.py

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from setuptools import setup
 
 setup(
     name="game-manager-lib",
-    version="0.1-Alpha",
+    version="0.1.1-Alpha",
     scripts=[],
-    packages=['gamemanagerlib',],
+    packages=['gamemanagerlib','gamemanagerlib.business', 'gamemanagerlib.storage'],
     author="Serguei Fedorov",
     url="https://github.com/SergueiFedorov/gamemanagerlib"
 )
