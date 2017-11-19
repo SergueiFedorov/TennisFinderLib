ecde92e7440781ab9e9a85f83c935eb10ec253e1
Serguei <blackraven36@gmail.com>
2017-11-19 17:33:25 -0600

    Adjusted setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ setup(
     name="game-manager-lib",
     version="0.1-Alpha",
     scripts=[],
+    packages=['gamemanagerlib',],
     author="Serguei Fedorov",
     url="https://github.com/SergueiFedorov/gamemanagerlib"
 )
