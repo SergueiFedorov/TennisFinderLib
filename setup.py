f657fab3902f76aa6c13e9232579c7e107e92a1a
Serguei <blackraven36@gmail.com>
2017-11-19 19:41:46 -0600

    Incremented version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="game-manager-lib",
-    version="0.1.3-Alpha",
+    version="0.1.4-Alpha",
     scripts=[],
     packages=['gamemanagerlib','gamemanagerlib.business', 'gamemanagerlib.storage'],
     author="Serguei Fedorov",
